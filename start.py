85476e0bf7d74644f259d5d503dc8e9f444f5ec1
Christian <christian.oyn.naversen@gmail.com>
2014-06-27 10:40:26 +0200

    test

---
+++ start.py
@@ -0,0 +1,15 @@
+
+from __future__ import division     # To assure that division between two integers gives a sensible result
+import numpy  as np                 # A (big) library for doing array oriented numerics
+import scipy.io as io
+import matplotlib as mpl            # A plotting framework 
+#from matplotlib import rc           # Configuration files
+import matplotlib.pyplot as plt # A plotting framework similar to MATLAB
+
+
+def main(argv):
+
+	print "This is awesome!!!<3"
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
