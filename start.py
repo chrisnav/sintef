a864b90d70cc640c9ddf953cddfca5da7c109ebd
Christian <christian.oyn.naversen@gmail.com>
2014-06-27 15:04:57 +0200

    First draft of the classes!

--- start.py
+++ start.py
@@ -7,13 +7,64 @@ import matplotlib as mpl            # A plotting framework
 #from matplotlib import rc           # Configuration files
 import matplotlib.pyplot as plt # A plotting framework similar to MATLAB
 
+class Network:
 
-def main(argv):
-
-	print "This is awesome!!!<3"
-	for i in range(10):
-		print i
+	def __init__ (self,filenameInput):  ##input = results.mat file from the simulation
+		self.resultsDict = io.loadmat(filenameInput)
+		## Add the nodes in the grid:
+		
+		self.nodes=[] ##nodes in the network
+		
+		##create list with ll the stuff needed to create an object of the Node class
+		
+		for i in listWithAllTheStuff: 
+			newNode=Node(i[0],i[1],i[2],i[3],i[4])
+			self.nodes.append(newNode)
+		
+		
+		for i in listOfAllTheBranchRelatedStuff:
+			
+		##Add all the connections...
+		
+		##Do more stuff!
+		
+		
+class Node:
+	def __init__ (self, country, number, load, generation, genCost)
+		self.country=country
+		self.number=number
+		self.load=load    #load as in power demand
+		self.generation=generation
+		self.genCost=genCost
+		self.loadCost=loadCost
+		self.connections=[]
 	
+	def calcProfit(self):
+		pass
+		## calculate the consumer and producer profit for the nodes home country, and calculate consumer profit for the countries who import power from this node
+	
+	def addNewConnection(self,nodeNumber,capacity,flow):  ##Connections are one-way! This is FROM the "self-node" TO the node with node number "nodeNumber"
+		self.connections.append([nodeNumber,capacity,flow])
+	
+	def updateConnection(self,nodeNumber,capacity,flow):
+		index = connectionIndex(nodeNumber)
+		if(index==-1):
+			print "The branch does not exist!"
+			return
+			
+		self.connections[index]=[nodeNumber,capacity,flow]
+	
+	def connectionIndex(self,nodeNumber):
+		index=0
+		for i in self.connections:
+			if(i[0]==nodeNumber):
+				return index 
+			index+=1
+		
+		return -1
+		
+def main(argv):
+	pass
 	
 	
 	
